c90a1ab4f14f5bccf86f50b399194a2d3ea2828c
phukq <phukq@cardano-labo.com>
2017-11-18 21:39:49 +0700

    Update code

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,9 +8,20 @@ contract Token {
 
     address owner;
     uint price = 100; // 1 ETH = 100 Token
+    address public admin;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
 
+    // Modifier will change behavior of a function
+    // The function will execute code in the modifier
+    // before execute its own codes.
+    modifier onlyOwner(address _addr) {
+        require(_addr == owner);
+        _;
+    }
+
+    // Constructor
+    // This function will be called when deploying
     function Token(uint256 _totalSupply) public {
         totalSupply = _totalSupply;
         availableNumber = _totalSupply;
@@ -19,6 +30,9 @@ contract Token {
         owner = msg.sender;
     }
 
+    // Fallback function
+    // This function will be called when some account send ether
+    // to this contract
     function () payable public {
         // Calculate amount of token
         uint256 amount = msg.value * price / 1 ether;
@@ -34,14 +48,23 @@ contract Token {
         availableNumber -= amount;
 
         // Refund eth
-        msg.sender.transfer(msg.value - amount*1 ether/price);
+        uint cost = amount*1 ether/price;
+        msg.sender.transfer(msg.value - cost);
+
+        // Transfer eth to admin
+        admin.transfer(cost);
 
         // Emit event
-        Transfer(0x0, msg.sender,amount*1 ether/price);
+        Transfer(0x0, msg.sender, amount);
+    }
+
+    function changeAdmin(address _adminAddress) onlyOwner(msg.sender) public {
+        admin = _adminAddress;
     }
 
     // Constant function that return token balance of an address
     function balanceOf(address _addr) public constant returns (uint256) {
         return balances[_addr];
     }
+
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Token = artifacts.require("./Token.sol");
+var Admin = artifacts.require("./Admin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Token, 1000);
+  deployer.deploy(Token, 200).then(()=>{
+    return deployer.deploy(Admin)
+  })
 };
